48f9d5e3ba6e8de457a15f58fd485c6cd64d5651
Quinn Shultz <vanoxite@gmail.com>
2022-12-31 11:03:28 -0500

    Added Part 1 source code.

---
+++ Part 1/companion/index.js
@@ -0,0 +1,21 @@
+import { geolocation } from "geolocation";
+
+/*
+ * Entry point for the companion app
+ */
+console.log("Companion code started");
+
+var current_location;
+
+geolocation.getCurrentPosition(locationSuccess, locationError, {
+  timeout: 60 * 1000
+});
+
+function locationSuccess(position) {
+  current_location = position.coords.latitude + ", " + position.coords.longitude
+  console.log("Current location: " + current_location);
+}
+
+function locationError(error) {
+  console.log("Error: " + error.code, "Message: " + error.message);
+}
